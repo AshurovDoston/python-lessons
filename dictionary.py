b0fc6ee75cb004499278048f819c5f546c9e688e
Doston Ashurov <A3209490-MSP01@ey.net>
2022-09-24 18:40:00 +0500

    add py files

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 17:37:10 2022
+
+@author: FF246WR
+"""
+
---
+++ lessons.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 17:38:40 2022
+
+@author: FF246WR
+"""
+
+# =============================================================================
+# menu = ['osh', 'manti', 'shashlik', 'norin', 'tandir']
+# orders = ['osh', 'somsa', 'manti', 'baliq']
+# 
+# if orders:
+#     for meal in orders:
+#         if meal in menu:
+#             print(f"We have {meal} in our Menu")
+#         else:
+#             print(f"Sorry, we don't have {meal} in our Menu")
+# else:
+#     print("There no meals in your cart")
+# =============================================================================
+    
+yosh = int(input("Yoshingizni kiriting! \n>>>"))
+if yosh>65: print("Siz COvID-19 risk guruhida ekansiz!")
+
+
+# =============================================================================
+# ism = input('What is your name?\n>>>')
+# if ism.lower() != 'ali':
+#     print(f"Uzr {ism.title}, biz Alini kutyapmiz!")
+# else:
+#     print(f"Salom {ism.title}")
+# =============================================================================
+     
+# =============================================================================
+# cars = ['audi', 'nissan', 'bmw', 'toyota', 'tesla', 'hyundai']
+# 
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# =============================================================================
+
+# =============================================================================
+# guests = ['Ali', 'Vali', 'Hasan', 'Husan', 'Fotima', 'Zuhra']
+# 
+# for guest in guests:
+#     print(f"Hurmatli {guest}, sizni va oila a'zolaringizni 16-oktabr kuni bo'ladigan nikoh to'yiga taklif etamiz.")
+#     print("Hurmat bilan Omonovlar oilasi \n")
+#     
+# =============================================================================
+
+# =============================================================================
+# nums = list(range(1, 11))
+# nums_squared = []
+# for num in nums:
+#     nums_squared.append(num**2)
+#     
+# 
+# friends = []
+# print("5 ta yaqin do'stingizni kiriting!")
+# for n in range(5):
+#     friends.append(input(f"{n+1}-do'stingizning ismini kiriting!"))
+# =============================================================================
