dbe11be3ea352e09772abd3e8e4d110504103af4
Doston Ashurov <A3209490-MSP01@ey.net>
2022-09-30 07:46:49 +0500

    lists dictionaries and while

---
+++ while_and_list.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 15:10:11 2022
+
+@author: FF246WR
+"""
+
+# =============================================================================
+# print("We'll make a list of your close friends")
+# names = []
+# n = 1
+# while True:
+#     prompt = f"Enter your {n}-friend's name: "
+#     name = input(prompt)
+#     names.append(name)
+#     repeat = input("Will you add more names? (yes/no)")
+#     n+=1
+#     if repeat != 'yes':
+#         break
+# 
+# for name in names:
+#     print(name.title())
+# =============================================================================
+    
+
+# =============================================================================
+# print("Save your friends' ages!")
+# friends = {}
+# flag = True
+# while flag:
+#     name = input("Enter your friend's name: ")
+#     age = input(f"Enter {name.title()}'s age: ")
+#     friends[name] = int(age)
+#     
+#     answer = input("Will you add more info? (yes/no) ")
+#     if answer == 'no':
+#         flag = False
+#         
+# for name, age in friends.items():
+#     print(f"{name.title()}'s age in {age}")
+# =============================================================================
+    
+    
+# =============================================================================
+# cars = ['lacetti', 'nexia', 'damas', 'tico', 'damas', 'cobat', 'damas']
+# car = 'damas'
+# while car in cars:
+#     cars.remove(car)
+# print(cars)
+# =============================================================================
+
+
+# =============================================================================
+# students = ['hasan', 'husan', 'olim', 'zokir', 'ashish']
+# marked_students = {}
+# while students:
+#     student = students.pop()
+#     mark = input(f"Enter {student.title()}'s mark: ")
+#     print(f"{student.title()} has been marked!")
+#     marked_students[student] = int(mark)
+# print(students)
+# print(marked_students)
+# =============================================================================
+
+
+cart = []
+while True:
+    
